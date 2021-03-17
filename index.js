56445273ce6dfec16375de8d825d86ebece40f3d
luis <foreverluiscastro@gmail.com>
2021-03-17 17:15:09 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickUp) {
+    return Math.abs(pickUp - 42);
+}
+
+function distanceFromHqInFeet(pickUp) {
+    return Math.abs(pickUp - 42) * 264;
+}
+
+function distanceTravelledInFeet(pickUp, dropOff) {
+    return Math.abs(pickUp - dropOff) * 264;
+}
+
+function calculatesFarePrice(pickUp, dropOff) {
+    if ((Math.abs(pickUp - dropOff) * 264) < 400) {
+        return 0;
+    } else if ((Math.abs(pickUp - dropOff) * 264) > 2500) {
+        return `cannot travel that far`;
+    } else if ((Math.abs(pickUp - dropOff) * 264) < 2000) {
+        return ((Math.abs(pickUp - dropOff) * 264) - 400)* .02;
+    } else if ((Math.abs(pickUp - dropOff) * 264) > 2000) {
+        return 25;
+    }
+
+}
